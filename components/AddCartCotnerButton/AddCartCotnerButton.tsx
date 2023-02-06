9b2e0d5dc0efd49223a5661ba659ebffde3cd059
vladimirHlobshastyi <vladimirHlobshastyi@gmail.com>
2023-02-06 20:00:39 +0200

    add components

--- components/AddCartCotnerButton/AddCartCotnerButton.tsx
+++ components/AddCartCotnerButton/AddCartCotnerButton.tsx
@@ -1,14 +1,38 @@
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai'
 import useAppSelector from '../../hooks/useAppSelector'
 import useIsProductInCart from '../../hooks/useIsProductInCart'
+import useRootDispatch from '../../hooks/useRootDispatch'
+import { addProduct, minusProduct } from '../../store/cart/cartSlice'
 import { dataCartProduct } from '../../store/cart/cartSlice.types'
 import style from './AddCartCotnerButton.module.scss'
+import { OrderCartButton, OrderCheckButton } from './AddCartCotnerButton.types'
 
+const OrderCheckButton = ({ productCount, dispatch, product }: OrderCheckButton) => {
+    return <div className={style.orderNumber}>
+        <div className={style.orderNumberMinus}
+            onClick={() => dispatch(minusProduct(product))}><AiOutlineMinus /></div>
+        <div className={style.orderNumberCall}><span>В корзине {productCount} шт</span> <span>перейти</span>{/* {countProduct ? countProduct() : null} */}</div>
+        <div className={style.orderNumberPlus}
+            onClick={() => dispatch(addProduct(product))}><AiOutlinePlus /></div>
+    </div>
 
+}
+
+const OrderCartButton = ({ dispatch, product }: OrderCartButton) => {
+    return <div className={style.cartButton} onClick={() => dispatch(addProduct(product))} ><span >В корзину</span></div>
+}
 const AddCartCotnerButton = ({ product }: { product: dataCartProduct }) => {
     const products = useAppSelector((state) => state.cart.products)
+    const dispatch = useRootDispatch()
     const countItemInCart = useIsProductInCart(products, product)
+
     return <div className={style.container}>
-        {countItemInCart ? countItemInCart : <span>В корзину</span>}
+        {countItemInCart ? <OrderCheckButton productCount={countItemInCart}
+            dispatch={dispatch}
+            product={product} /> :
+            <OrderCartButton
+                dispatch={dispatch}
+                product={product} />}
     </div>
 }
 
