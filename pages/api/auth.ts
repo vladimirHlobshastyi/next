4d0fe991ec3794a38f6602319fb18d6a12766be2
vladimirHlobshastyi <vladimirHlobshastyi@gmail.com>
2023-03-06 11:16:03 +0200

    add api

--- pages/api/auth.ts
+++ pages/api/auth.ts
@@ -8,7 +8,6 @@ const loginMock = "user";
 const passwordMock: string = "password";
 const token = authToken;
 
-// обработчик запроса для авторизации пользователя
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== "POST") {
     return res.status(405).json({ message: "Method not allowed" });
