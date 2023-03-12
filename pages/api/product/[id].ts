994ddd79153c41f0edc7f79943a61c600b0e7c69
vladimirHlobshastyi <vladimirHlobshastyi@gmail.com>
2023-03-12 17:55:32 +0200

    fixed api

---
+++ pages/api/product/[id].ts
@@ -0,0 +1,28 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { allProducts, produtsDataType } from "../../../moc/moc";
+
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { category, idProduct } = req.query;
+
+  if (!category || !idProduct || Array.isArray(category)) {
+    return res
+      .status(400)
+      .json({ message: "Missing category or idProduct parameter" });
+  } else if (!allProducts[category]) {
+    return res.status(404).json({ message: "Category not found" });
+  }
+
+  if (idProduct) {
+    const product = allProducts[category].data.find(
+      (item) => item.id === idProduct
+    );
+    if (product) {
+      return res.status(200).json(product);
+    } else {
+      return res.status(404).json({ message: "Product not found" });
+    }
+  } else {
+    return res.status(200).json(allProducts[category].data);
+  }
+}
--- pages/api/products/[id].ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import { mocProducts } from "../../../moc/moc";
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { id } = req.query;
-  const product = mocProducts.filter((item) => item.id.toString() === id);
-  res.status(200).json(product[0]);
-}
