eb00e3875c5457e733590be1646d18a4edcc12e0
vladimirHlobshastyi <vladimirHlobshastyi@gmail.com>
2023-03-30 13:23:25 +0300

    add useControlCount hook

---
+++ hooks/useControlCount.tsx
@@ -0,0 +1,8 @@
+import { useMemo } from 'react';
+import ControlCountComponent from '../components/ControlCountComponent/ControlCountComponent';
+
+function useControlCount(totalCount: number) {
+    return useMemo(() => totalCount ? <ControlCountComponent countProductsInCart={totalCount} /> : null, [totalCount]);
+}
+
+export default useControlCount;
