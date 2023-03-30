335b7bcc533ab689d2ddba453a9ab385c6633759
vladimirHlobshastyi <vladimirHlobshastyi@gmail.com>
2023-03-30 15:05:33 +0300

    changed settings

--- next.config.js
+++ next.config.js
@@ -12,7 +12,6 @@ const nextConfig = {
 module.exports = nextConfig;
 module.exports = {
   env: {
-    /* "http://localhost:3001", */
     API_URL:
       /*  "http://localhost:3000"  */
       "https://backend-for-next.vercel.app",
