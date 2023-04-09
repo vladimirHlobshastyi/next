f2d268f3887ce7a348a9c0e82ba9252981ae1678
vladimirHlobshastyi <vladimirHlobshastyi@gmail.com>
2023-04-09 13:37:21 +0300

    refactored login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,55 +1,70 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { useForm } from "react-hook-form";
 import style from "./../styles/login.module.scss";
 import useRootDispatch from "../hooks/useRootDispatch";
-import { isAuthState, loginThunk } from "../store/auth/authSlice";
+import { errorAuthState, isAuthState, loginThunk } from "../store/auth/authSlice";
 import useAppSelector from "../hooks/useAppSelector";
 import { useRouter } from "next/router";
 
+const FAILED_TO_LOGIN = 'Failed to login'
+
 const Login = () => {
     const [showPassword, setShowPassword] = useState(false);
+
     const router = useRouter()
+    const dispatch = useRootDispatch();
+
     const isAuth = useAppSelector(isAuthState)
+    const errorAuth = useAppSelector(errorAuthState)
+
     const {
         register,
         handleSubmit,
+        reset,
         formState: { errors },
     } = useForm<{ login: string; password: string }>();
 
-    const dispatch = useRootDispatch();
+    const isLoginFailed = errorAuth.message === FAILED_TO_LOGIN;
 
-    const onSubmit = (data: { login: string; password: string }) => {
+    const onSubmit = useCallback((data: { login: string; password: string }) => {
         dispatch(loginThunk(data));
-    };
+        reset()
+    }, [dispatch]);
 
     const toggleShowPassword = () => {
         setShowPassword(!showPassword);
     };
 
-    useEffect(() => { if (isAuth) { router.push('/user/history_of_orders') } }, [isAuth])
+    useEffect(() => {
+        if (isAuth) {
+            router.push('/user/history_of_orders')
+        }
+    }, [isAuth, router]);
 
     return (
         <div className={style.container}>
-            <h1>Login</h1>
+            <h1>Вход в личный кабинет</h1>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <div>
-                    <label htmlFor="login">Login:</label>
-                    <input type="text" id="login" {...register("login", { required: true })} />
-                    {errors.login && <span>This field is required</span>}
+                    <label htmlFor="login">Логин:</label>
+                    <input type="text" placeholder="логин &rarr; user" id="login" {...register("login", { required: true })} />
+                    {errors.login && <span>Введите логин</span>}
                 </div>
                 <div>
-                    <label htmlFor="password">Password:</label>
+                    <label htmlFor="password">Пароль:</label>
                     <input
                         type={showPassword ? "text" : "password"}
                         id="password"
+                        placeholder="пароль &rarr; password"
                         {...register("password", { required: true })}
                     />
                     <button type="button" onClick={toggleShowPassword}>
-                        {showPassword ? "Hide" : "Show"}
+                        {showPassword ? "показать" : "скрыть"}
                     </button>
-                    {errors.password && <span>This field is required</span>}
+                    {errors.password && <span>Введите пароль</span>}
                 </div>
-                <button type="submit">Login</button>
+                <div className={style.error}>{errorAuth.isError && (isLoginFailed ? <span>Не верный логин или пароль</span> : <span>Логинизация не удалась... пожалуйста попробуйте позже</span>)}</div>
+                <button type="submit">Принять</button>
             </form>
         </div>
     );
