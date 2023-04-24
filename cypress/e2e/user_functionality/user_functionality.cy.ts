a54aacd93a9c7edef0935cc2e5a9f16b124ff7d7
vladimirHlobshastyi <vladimirHlobshastyi@gmail.com>
2023-04-24 20:30:24 +0300

    added new test blog

--- cypress/e2e/user_functionality/user_functionality.cy.ts
+++ cypress/e2e/user_functionality/user_functionality.cy.ts
@@ -1,3 +1,5 @@
+import { responsBlogType } from "./../../../pages/api/blog/[page]";
+import { Interception } from "cypress/types/net-stubbing";
 import "../../support/commands";
 /// <reference types="cypress" />
 
@@ -34,5 +36,32 @@ describe("user_functionality", () => {
     cy.intercept("GET", "/blog/1").as("blog");
     cy.visit("/blog/1");
     cy.wait("@blog").its("response.statusCode").should("eq", 200);
+  });
+
+  it("should successfully open all posts at posts category", () => {
+    cy.intercept("GET", "/api/blog/1").as("blogRequest");
+
+    cy.request({
+      method: "GET",
+      url: "/api/blog/1",
+    })
+      .then((request) => {
+        return request.body;
+      })
+      .then((bodyRespons: responsBlogType) => {
+        const totalPage = bodyRespons.totalPages;
+        expect(totalPage).to.be.a("number");
+        expect(totalPage).to.be.at.least(1);
+
+        for (let page = 1; page <= totalPage; page++) {
+          cy.visit(`/blog/${page}`);
+          cy.get("[data-cy-blog-id]").each((blog) => {
+            const blogId = blog.attr("data-cy-blog-id");
+            cy.wrap(blog).click();
+            cy.url().should("include", `/blog/1/${blogId}`);
+            cy.go("back");
+          });
+        }
+      });
   });
 });
