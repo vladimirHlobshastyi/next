5766bceafb90a5e0e8e39a40ca60df05179fc9f8
vladimirHlobshastyi <vladimirHlobshastyi@gmail.com>
2023-05-22 10:54:50 +0300

    Refactored eslint settings: changed eslint rulles

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,15 @@
 module.exports = {
-  extends: ['eslint-config-airbnb'],
+  extends: ['prettier', 'next'],
   plugins: ['import', 'jsx-a11y', 'react', 'react-hooks', 'prettier'],
-  /*  rules: {
-    'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'react/jsx-filename-extension': [
-      1,
-      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
-    ],
-    'react/jsx-props-no-spreading': 'off',
-    'import/extensions': 'off',
-    'import/no-unresolved': 'off',
-    'import/prefer-default-export': 'off',
-    'react-hooks/rules-of-hooks': 'warn',
-    'react-hooks/exhaustive-deps': 'warn',
-  }, */
   rules: {
+    'import/extensions': 'off',
+    'react-hooks/exhaustive-deps': 'off',
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
     'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
     'react/jsx-props-no-spreading': 'off',
-    'import/extensions': [
-      'error',
-      'ignorePackages',
-      {
-        js: 'never',
-        jsx: 'never',
-        ts: 'never',
-        tsx: 'never',
-      },
-    ],
     'import/no-unresolved': 'off',
     'import/prefer-default-export': 'off',
-    'react-hooks/rules-of-hooks': 'warn',
-    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'off',
   },
 };
